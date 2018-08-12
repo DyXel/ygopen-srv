3fe74989618f134cd899d542fea217e2351d8e22
Dylam De La Torre <dyxel04@gmail.com>
2018-08-12 17:57:41 +0200

    move msg operations to their own functions

--- team_duel_observer.cpp
+++ team_duel_observer.cpp
@@ -5,21 +5,6 @@
 
 #include <set>
 
-TeamDuelObserver::TeamDuelObserver(short team) :
-	team(team),
-	responseFlag(false)
-{}
-
-void TeamDuelObserver::AddPlayer(int pos, std::shared_ptr<ServerRoomClient> player)
-{
-	players.insert(std::make_pair(pos, player));
-}
-
-void TeamDuelObserver::ClearPlayers()
-{
-	players.clear();
-}
-
 static const std::set<int> playerMsgs =
 {
 	CoreMessage::SelectBattleCmd,
@@ -58,30 +43,43 @@ static const std::set<int> knowledgeMsgs =
 	CoreMessage::Draw
 };
 
+TeamDuelObserver::TeamDuelObserver(short team) :
+	team(team),
+	responseFlag(false)
+{}
+
+void TeamDuelObserver::AddPlayer(int pos, std::shared_ptr<ServerRoomClient> player)
+{
+	players.insert(std::make_pair(pos, player));
+}
+
+void TeamDuelObserver::ClearPlayers()
+{
+	players.clear();
+}
+
 const bool TeamDuelObserver::IsReponseFlagSet() const
 {
 	return responseFlag;
 }
 
-void TeamDuelObserver::OnNotify(void* buffer, size_t length)
+bool TeamDuelObserver::IsMsgForThisTeam(void* buffer, size_t length)
 {
-	responseFlag = false;
 	BufferManipulator bm(buffer, length);
-
 	const auto msgType = bm.Read<uint8_t>();
 	
 	// Check for blacklisted message
 	if(msgType == CoreMessage::Win)
-		return;
+		return false;
 
 	// Check for messages to this team
 	const auto search = playerMsgs.find((int)msgType) != playerMsgs.end();
 	if(search)
 	{
 		if(bm.Read<uint8_t>() == team)
-			responseFlag = true;
+			return (responseFlag = true);
 		else
-			return;
+			return false;
 	}
 	else
 	{
@@ -92,9 +90,9 @@ void TeamDuelObserver::OnNotify(void* buffer, size_t length)
 			{
 				bm.Forward(1);
 				if(bm.Read<uint8_t>() == team)
-					responseFlag = true;
+					return (responseFlag = true);
 				else
-					return;
+					return false;
 			}
 			break;
 			case CoreMessage::Hint:
@@ -102,9 +100,9 @@ void TeamDuelObserver::OnNotify(void* buffer, size_t length)
 				const auto type = bm.Read<uint8_t>();
 				const auto forPlayer = bm.Read<uint8_t>();
 				if((type == 1 || type == 2 || type == 3 || type == 5) && forPlayer != team)
-					return;
+					return false;
 				else if ((type == 4 || type == 6 || type == 7 || type == 8 || type == 9) && forPlayer == team)
-					return;
+					return false;
 				// type == 10 // always send the message
 			}
 			break;
@@ -113,147 +111,160 @@ void TeamDuelObserver::OnNotify(void* buffer, size_t length)
 				const auto forPlayer = bm.Read<uint8_t>();
 				bm.Forward(1 + 4);
 				if (bm.Read<uint8_t>() == 0x01 && forPlayer != team) // 0x01 == LOCATION_DECK
-					return;
+					return false;
 			}
 			break;
 		}
 	}
 
-	// Knowledge stripping
-	std::string newMsg;
-	const auto search2 = knowledgeMsgs.find((int)msgType) != knowledgeMsgs.end();
-	if(search2)
+	return true;
+}
+
+bool TeamDuelObserver::StripKnowledge(void* buffer, size_t length, std::string& newMsg)
+{
+	newMsg.reserve((size_t)length);
+	std::memcpy(&newMsg[0], buffer, length);
+	BufferManipulator bm(&newMsg[0], length);
+
+	const auto msgType = bm.Read<uint8_t>();
+
+	if(knowledgeMsgs.find((int)msgType) == knowledgeMsgs.end())
+		return false;
+
+	auto DeleteNonPublicKnowledge = [&bm](const uint8_t forPlayer)
 	{
-		newMsg.reserve((size_t)length);
-		std::memcpy(&newMsg[0], buffer, length);
-		BufferManipulator bm2(&newMsg[0], length);
-
-		bm2.Forward(1); // msgType
-
-		auto DeleteNonPublicKnowledge = [&bm2](const uint8_t forPlayer)
+		const auto count = bm.Read<uint32_t>();
+		for(uint32_t i = 0; i < count; i++)
 		{
-			const auto count = bm2.Read<uint32_t>();
+			bm.Forward(4); // code
+			const auto controler = bm.Read<uint8_t>();
+			if(forPlayer != controler)
+			{
+				bm.Backward(4 + 1);
+				bm.Write<uint32_t>(0);
+				bm.Forward(1); // controler
+			}
+			bm.Forward(9); // loc_info - controler
+		}
+	};
+	
+	switch(msgType)
+	{
+		case CoreMessage::SelectCard:
+		{
+			const auto forPlayer = bm.Read<uint8_t>();
+			bm.Forward(3);
+			DeleteNonPublicKnowledge(forPlayer);
+		}
+		break;
+		case CoreMessage::SelectTribute:
+		{
+			const auto forPlayer = bm.Read<uint8_t>();
+			bm.Forward(3);
+			const auto count = bm.Read<uint32_t>();
 			for(uint32_t i = 0; i < count; i++)
 			{
-				bm2.Forward(4); // code
-				const auto controler = bm2.Read<uint8_t>();
+				bm.Forward(4); // code
+				const auto controler = bm.Read<uint8_t>();
 				if(forPlayer != controler)
 				{
-					bm2.Backward(4 + 1);
-					bm2.Write<uint32_t>(0);
-					bm2.Forward(1); // controler
+					bm.Backward(4 + 1);
+					bm.Write<uint32_t>(0);
+					bm.Forward(1); // controler
 				}
-				bm2.Forward(9); // loc_info - controler
+				bm.Forward(6); // ???
 			}
-		};
-		
-		switch(msgType)
-		{
-			case CoreMessage::SelectCard:
-			{
-				const auto forPlayer = bm2.Read<uint8_t>();
-				bm2.Forward(3);
-				DeleteNonPublicKnowledge(forPlayer);
-			}
-			break;
-			case CoreMessage::SelectTribute:
-			{
-				const auto forPlayer = bm2.Read<uint8_t>();
-				bm2.Forward(3);
-				const auto count = bm2.Read<uint32_t>();
-				for(uint32_t i = 0; i < count; i++)
-				{
-					bm2.Forward(4); // code
-					const auto controler = bm2.Read<uint8_t>();
-					if(forPlayer != controler)
-					{
-						bm2.Backward(4 + 1);
-						bm2.Write<uint32_t>(0);
-						bm2.Forward(1); // controler
-					}
-					bm2.Forward(6); // ???
-				}
-			}
-			break;
-			case CoreMessage::SelectUnselect:
-			{
-				const auto forPlayer = bm2.Read<uint8_t>();
-				bm2.Forward(4);
-				DeleteNonPublicKnowledge(forPlayer);
-				DeleteNonPublicKnowledge(forPlayer);
-			}
-			break;
-			case CoreMessage::ShuffleHand:
-			case CoreMessage::ShuffleExtra:
-			{
-				const auto forPlayer = bm2.Read<uint8_t>();
-				if(forPlayer != team)
-				{
-					const auto count = bm2.Read<uint8_t>();
-					for(uint32_t i = 0; i < count; i++)
-						bm2.Write<uint32_t>(0);
-				}
-			}
-			break;
-			case CoreMessage::Move:
-			{
-				// TODO: use enums for positions and locations
-				auto IsCardPublic = [](const uint8_t location, const uint32_t position) -> const bool
-				{
-					if(location & (0x10 + 0x80) && !(location & (0x01 + 0x02)))
-						return true;
-					else if(!(position & 0x0a))
-						return true;
-					return false;
-				};
-
-				bm2.Forward(4); // code
-				bm2.Forward(10); // loc_info previous
-				const auto currentControler = bm2.Read<uint8_t>();
-				const auto currentLocation = bm2.Read<uint8_t>();
-				bm2.Forward(4); // loc_info.sequence
-				const auto currentPosition = bm2.Read<uint32_t>();
-				bm2.Forward(4); // ?
-
-				if(currentControler != team && !IsCardPublic(currentLocation, currentPosition))
-				{
-					bm2.Backward(4 + 10 + 10 + 4);
-					bm2.Write<uint32_t>(0);
-				}
-			}
-			break;
-			case CoreMessage::Set:
-			{
-				bm2.Write<uint32_t>(0);
-			}
-			break;
-			case CoreMessage::Draw:
-			{
-				const auto forPlayer = bm2.Read<uint8_t>();
-				if(forPlayer != team)
-				{
-					const auto count = bm2.Read<uint8_t>();
-					for(uint8_t i = 0; i < count; i++)
-					{
-						const auto code = bm2.Read<uint32_t>();
-						bm2.Backward(4);
-						if(code & 0x80000000) // if card is POS_FACEUP
-							bm2.Forward(4);
-						else
-							bm2.Write<uint32_t>(0);
-					}
-				}
-			}
-			break;
 		}
-	}
+		break;
+		case CoreMessage::SelectUnselect:
+		{
+			const auto forPlayer = bm.Read<uint8_t>();
+			bm.Forward(4);
+			DeleteNonPublicKnowledge(forPlayer);
+			DeleteNonPublicKnowledge(forPlayer);
+		}
+		break;
+		case CoreMessage::ShuffleHand:
+		case CoreMessage::ShuffleExtra:
+		{
+			const auto forPlayer = bm.Read<uint8_t>();
+			if(forPlayer != team)
+			{
+				const auto count = bm.Read<uint8_t>();
+				for(uint32_t i = 0; i < count; i++)
+					bm.Write<uint32_t>(0);
+			}
+		}
+		break;
+		case CoreMessage::Move:
+		{
+			// TODO: use enums for positions and locations
+			auto IsCardPublic = [](const uint8_t location, const uint32_t position) -> const bool
+			{
+				if(location & (0x10 + 0x80) && !(location & (0x01 + 0x02)))
+					return true;
+				else if(!(position & 0x0a))
+					return true;
+				return false;
+			};
 
-	// Send message
+			bm.Forward(4); // code
+			bm.Forward(10); // loc_info previous
+			const auto currentControler = bm.Read<uint8_t>();
+			const auto currentLocation = bm.Read<uint8_t>();
+			bm.Forward(4); // loc_info.sequence
+			const auto currentPosition = bm.Read<uint32_t>();
+			bm.Forward(4); // ?
+
+			if(currentControler != team && !IsCardPublic(currentLocation, currentPosition))
+			{
+				bm.Backward(4 + 10 + 10 + 4);
+				bm.Write<uint32_t>(0);
+			}
+		}
+		break;
+		case CoreMessage::Set:
+		{
+			bm.Write<uint32_t>(0);
+		}
+		break;
+		case CoreMessage::Draw:
+		{
+			const auto forPlayer = bm.Read<uint8_t>();
+			if(forPlayer != team)
+			{
+				const auto count = bm.Read<uint8_t>();
+				for(uint8_t i = 0; i < count; i++)
+				{
+					const auto code = bm.Read<uint32_t>();
+					bm.Backward(4);
+					if(code & 0x80000000) // if card is POS_FACEUP
+						bm.Forward(4);
+					else
+						bm.Write<uint32_t>(0);
+				}
+			}
+		}
+		break;
+	}
+	
+	return true;
+}
+
+void TeamDuelObserver::OnNotify(void* buffer, size_t length)
+{
+	responseFlag = false;
+	
+	if(!IsMsgForThisTeam(buffer, length))
+		return;
+
 	STOCMessage msg(STOC_GAME_MSG);
-	if(!search2)
-		msg.GetBM()->Write(std::make_pair(buffer, length));
-	else
+	
+	std::string newMsg;
+	if(StripKnowledge(buffer, length, newMsg))
 		msg.GetBM()->Write(std::make_pair((void*)&newMsg[0], length));
+	else
+		msg.GetBM()->Write(std::make_pair(buffer, length));
 	msg.Encode();
 
 	for(auto& player : players)
--- team_duel_observer.hpp
+++ team_duel_observer.hpp
@@ -2,6 +2,7 @@
 #define __TEAM_DUEL_OBSERVER_HPP__
 #include <map>
 #include <memory>
+#include <string>
 #include "duel_observer.hpp"
 
 class ServerRoomClient;
@@ -11,13 +12,16 @@ class TeamDuelObserver : public DuelObserver
 	short team; // 0 or 1
 	bool responseFlag;
 	std::map<int, std::shared_ptr<ServerRoomClient>> players;
+	
+	bool IsMsgForThisTeam(void* buffer, size_t length);
+	bool StripKnowledge(void* buffer, size_t length, std::string& newMsg);
 public:
 	TeamDuelObserver(short team);
 
+	virtual void OnNotify(void* buffer, size_t length);
 	void AddPlayer(int pos, std::shared_ptr<ServerRoomClient> player);
 	void ClearPlayers();
 	const bool IsReponseFlagSet() const;
-	virtual void OnNotify(void* buffer, size_t length);
 };
 
 #endif // __TEAM_DUEL_OBSERVER_HPP__
