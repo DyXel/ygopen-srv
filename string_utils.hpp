c329beadb3576964a0f5a1550eedbfed54444b26
edo9300 <edoardo762@gmail.com>
2019-01-03 20:16:14 +0100

    Fixed windows build

--- string_utils.hpp
+++ string_utils.hpp
@@ -8,18 +8,29 @@
 // functions for the legacy server.
 // Do not use them outside of the legacy server.
 
-namespace su
-{
-	static std::wstring_convert<std::codecvt_utf8<char16_t>, char16_t> ucs2conv;
+namespace su {
+#if _MSC_VER >= 1900
 
-	static std::string u16tos(std::u16string u16)
-	{
-		return ucs2conv.to_bytes(u16);
-	}
+static std::wstring_convert<std::codecvt_utf8_utf16<int16_t>, int16_t> ucs2conv;
 
-	static std::u16string stou16(std::string s)
-	{
-		return ucs2conv.from_bytes(s);
-	}
+static std::string u16tos(const std::u16string& u16) {
+	auto p = reinterpret_cast<const int16_t *>(u16.data());
+	return ucs2conv.to_bytes(p, p + u16.size());
+}
+
+static std::u16string stou16(const std::string& s) {
+	return reinterpret_cast<const char16_t *>(ucs2conv.from_bytes(s).data());
+}
+#else
+static std::wstring_convert<std::codecvt_utf8<char16_t>, char16_t> ucs2conv;
+
+static std::string u16tos(const std::u16string& u16) {
+	return ucs2conv.to_bytes(u16);
+}
+
+static std::u16string stou16(const std::string& s) {
+	return ucs2conv.from_bytes(s);
+}
+#endif
 }
 #endif // __STRING_UTILS_HPP__
