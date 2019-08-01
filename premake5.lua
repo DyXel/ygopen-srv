34e5860b7ea8d3b7a816a1184b619e3099cb97d1
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2019-08-01 10:17:22 -0400

    Fix code style in premake

--- premake5.lua
+++ premake5.lua
@@ -1,49 +1,49 @@
 local project_name = "ygopen-srv"
 
 workspace(project_name)
-	language("C++")
-	location("build")
-	objdir("obj")
-
-	configurations({"Debug", "Release"})
+	language "C++"
+	location "build"
+	objdir "obj"
 	startproject(project_name)
 
-	filter("action:vs*")
-		characterset("ASCII")
+	configurations {"Debug", "Release"} 
+	
+	filter "configurations:Debug"
+		symbols "On"
+		defines "_DEBUG"
+		targetdir "bin/debug"
+		runtime "Debug"
+
+	filter "configurations:Release"
+		optimize "Speed"
+		defines "_RELEASE"
+		targetdir "bin/release"
+
+	filter "action:vs*"
+		characterset "ASCII"
 		platforms { "x86", "x86_64" }
 		filter "platforms:x86"
 			architecture "x32"
 		filter "platforms:x64"
 			architecture "x64"
 
-	filter("configurations:Debug")
-		symbols("On")
-		defines("_DEBUG")
-		targetdir("bin/debug")
-		runtime "Debug"
-
-	filter("configurations:Release")
-		optimize("Speed")
-		defines("_RELEASE")
-		targetdir("bin/release")
-
-	filter("system:windows")
+	filter "system:windows"
 		defines { "WIN32", "_WIN32", "NOMINMAX" }
 
-	filter("system:macosx")
+	filter "system:macosx"
 		includedirs "/usr/local/include"
 
 project(project_name)
-	kind("ConsoleApp")
-	warnings("Extra")
-	defines("ASIO_STANDALONE")
-	files({"**.hpp", "**.cpp"})
-	staticruntime "on"
+	kind "ConsoleApp"
 	cppdialect "C++17"
+	warnings "Extra"
+	defines "ASIO_STANDALONE"
+	files { "**.hpp", "**.cpp" } 
+	staticruntime "on"
 	
-	filter("system:not windows")
-		buildoptions({"-pedantic"})
-		links({"dl", "pthread", "sqlite3"})
+	filter "system:not windows"
+		buildoptions { "-pedantic" } 
+		links { "dl", "pthread", "sqlite3" } 
 
 local function vcpkgStaticTriplet(prj)
 	premake.w('<VcpkgTriplet Condition="\'$(Platform)\'==\'Win32\'">x86-windows-static</VcpkgTriplet>')
